45da0aa27aed2e847ba5373626f74ad9b42f5355
Tesfaye K Belay <99195378+TKbelay@users.noreply.github.com>
2022-02-07 16:47:52 +0100

    Update beta_new.jl

--- beta_new.jl
+++ beta_new.jl
@@ -1,18 +1,17 @@
-#run as julia beta_new.jl (#for julia version 1.4.2)
+#run as julia beta_new.jl (#for running on linux command line)
  
-using DelimitedFiles
-genot=trunc.(Int64,readdlm("genot_id.ped"))[:];#Read a list of genotyped animal id for 30729
-ngenot=trunc.(Int64,readdlm("ngenot_id.ped"))[:]; #Read a list on ungenotyped animal id
+using DelimitedFiles, SparseArrays, LinearAlgebra
+
+genot=trunc.(Int64,readdlm("genot_id.ped"))[:];#Read a list of genotyped animal ids
+ngenot=trunc.(Int64,readdlm("ngenot_id.ped"))[:]; #Read a list on ungenotyped animal ids
 
 Q_geno=readdlm("ggrpcont_new_genosort_bothid.dat"); #Read genetic group contribution for genotyped animals sorted based renum id
-Q=Q_geno[:,3:end];#307296x115
+Q2=Q_geno[:,3:end];# the first 2 coulmns might be Id and inbreeding coeffcient
 
-ped=trunc.(Int64,readdlm("renumped_nobd.ped"));
-#ped=trunc.(Int64,readdlm("renumped_valp0_nobd.ped"));
+ped=trunc.(Int64,readdlm("renumped_nobd.ped"));# read pedigree file (renumbered)
 animals=length(ped[:,1]);
 
-using SparseArrays
-using LinearAlgebra
+#construct inverse pedigree based relationship matrix following Handersons rule (inbreeding coefficient is not included in the A inv)
 IT=sparse(1.0*I,animals,animals);#calculate inverse of the lower triangular matrix (T) in A-matrix: (inv(A)=(inv(T))'*inv(D)*inv(T)
 ID=2*ones(animals);#calculate inverse of diagonal matrix(D), which is reciprocal of diagonal elements of A-matrix
 
@@ -35,18 +34,14 @@ for i=1:animals
 #elements in inverse T are finshed;
 end
 
-using LinearAlgebra
-
 Ainv=IT'*sparse(Diagonal(ID))*IT;
 
-IA11=Ainv[ngenot,ngenot];
-IA12=Ainv[ngenot,genot];
+IA11=Ainv[ngenot,ngenot];# extract block of Ainv for non-genotyped animals
+IA12=Ainv[ngenot,genot]; #extract block of Ainv for non-genotyped and genotyped animals
 
-J=-IA11\(IA12*ones(30729));
-Qplus=-IA11\(IA12*Q);
+J=-IA11\(IA12*ones(ng)); #calculate J factor, and ng is number of genotyped animals
+Qplus=-IA11\(IA12*Q2); #calculate genetic group contributions for non-genotyped animals (Qplus), but orginal genetic contribution (Q2) is used for genotyped animals
 
 writedlm("J_large_valp0.dat",J,"\t")
 
 writedlm("Qplus_matrix.dat",Qplus,"\t")
-
-#writedlm("Qplusnew_matrix_valp0.dat",J_Q,"\t")
